14158e5d15181ab0d2b9927f9c0134fb1e1a6a04
preciousogbeiwi <53301886+preciousogbeiwi@users.noreply.github.com>
2022-10-26 19:28:37 +0100

    Adding the setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='test_scraper_multiple_pages.py',
+    version='0.0.1',
+    description='package that allows you test the scrape_multiple_pages script',
+    url='https://github.com/preciousogbeiwi/WebScraper/tree/main/test_scraper',
+    author='Precious Ogbeiwi',
+    license='',
+    packages=find_packages(),
+    install_requires=['selenium'])
